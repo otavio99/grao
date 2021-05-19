fd27e434026b161463b183163be4aa1877cca5fb
otavio99 <42242424+otavio99@users.noreply.github.com>
2021-05-19 15:52:04 -0400

    Update utility.py

--- utility.py
+++ utility.py
@@ -3,11 +3,13 @@ print(">>> a Taxa Selic seja de 4,25% ao ano")
 print(">>> é pago 100% da Taxa Selic")
 print(">>> só é rentabilizado os dias úteis")
 print(">>> os impostos sobre os investimentos serão desconsiderados")
+print(">>> serão considerados 36 semanas para o investimento")
+print(">>> O investimento realizado seja de R$ 100,00") 
 
 t = 36
 i = 4.25/100
 
-p = float(input("Valor do investimento semanal: "))
+p = 100.00
 #36 semanas
 p = p*36
 
